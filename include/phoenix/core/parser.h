46d92edf541d74ac40a105ba1a286b73697a824d
yang <jzy991126@outlook.com>
2021-01-09 10:18:06 +0800

    add some declare

---
+++ include/phoenix/core/parser.h
@@ -0,0 +1,25 @@
+//
+// Created by jzy99 on 2021/1/3.
+//
+
+#include<tinyxml2.h>
+#include<phoenix/core/phoenix.h>
+
+
+#pragma once
+
+PHOENIX_NAMESPACE_BEGIN
+
+class SceneParser{
+public:
+
+    SceneParser();
+
+
+
+};
+
+
+PHOENIX_NAMESPACE_END
+
+
